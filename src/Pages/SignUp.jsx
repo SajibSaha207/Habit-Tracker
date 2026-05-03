f9c52ffab392fe3e503885e74c740432d9306777
SajibSaha207 <sajibsaha.207@gmail.com>
2026-05-04 01:31:28 +0600

    update signup page

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -7,6 +7,8 @@ import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import Footer from '../Components/Footer';
 import Navbar from '../Components/Navbar';
 
+import img2 from '../assets/login img2.1.png';
+
 
 const googleProvider = new GoogleAuthProvider();
 const SignUp = () => {
@@ -100,51 +102,89 @@ const handleTooglePasswordShow = (event)=>{
 }
 
     return (
-        <div className=''>
-            <Navbar></Navbar>
-        <div className='flex justify-center min-h-screen items-center mt-10 bg-base-300'>
-    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-        <h2 className='font-semibold text-2xl text-center py-5'>Create Your Account</h2>
-      <div className="card-body">
-      <form onSubmit={handleSignUp}>
-  <fieldset className="fieldset">
+<div className='flex flex-col min-h-screen'>
+  <Navbar />
 
-          <label className="label">Name</label>
-          <input type="text" name='name' className="input" placeholder="Name"  />
+  <div className='flex flex-1 items-center justify-center bg-purple-100 py-10 px-4 mt-15'>
 
-            <label className="label">Image_URL</label>
-          <input type="text" name='image_url' className="input" placeholder="Image_URL" />
-            
-          <label className="label">Email</label>
-          <input type="email" name='email' className="input" placeholder="Email" />
+    {/* MAIN CONTAINER */}
+    <div className='flex w-full max-w-5xl bg-white rounded-2xl shadow-2xl overflow-hidden'>
 
-     <label className="label">Password</label>
-      <div className='relative'>
-          <input type={showPassword ? 'text' : 'password'} name='password' className="input" placeholder="Password" />
-          <button onClick={handleTooglePasswordShow} className="btn btn-xs top-2 right-3 absolute">{showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>}</button>
-       </div>
-       
- 
-          <button className="btn btn-neutral mt-4">Sign Up</button>
+   {/* LEFT IMAGE SECTION */}
+<div className='hidden md:flex w-1/2 items-stretch'>
+  <img 
+    src={img2} 
+    alt="" 
+    className='w-full h-150 object-cover rounded-l-2xl'
+  />
+</div>
+
+      {/* RIGHT FORM SECTION */}
+      <div className='w-full md:w-1/2 p-6'>
+
+        <div className="card bg-base-100 w-full shadow-none">
+          <h2 className='font-semibold text-2xl text-center py-5'>Create Your Account</h2>
+
+          <div className="card-body">
+            <form onSubmit={handleSignUp}>
+              <fieldset className="fieldset">
+
+                <label className="label">Name</label>
+                <input type="text" name='name' className="input" placeholder="Name" />
+
+                <label className="label">Image_URL</label>
+                <input type="text" name='image_url' className="input" placeholder="Image_URL" />
+
+                <label className="label">Email</label>
+                <input type="email" name='email' className="input" placeholder="Email" />
+
+                <label className="label">Password</label>
+
+                <div className='relative mr-22'>
+                  <input 
+                    type={showPassword ? 'text' : 'password'} 
+                    name='password' 
+                    className="input" 
+                    placeholder="Password" 
+                  />
+                  <button 
+                    onClick={handleTooglePasswordShow} 
+                    className="btn btn-xs absolute top-2 right-3"
+                  >
+                    {showPassword ? <FaEyeSlash /> : <FaEye />}
+                  </button>
+                </div>
+
+                  <button className="btn btn-neutral mt-4">Sign Up</button>
           <button onClick={handleGoogleSignIn} className="btn bg-white text-black border-[#e5e5e5]">
   <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
   Login with Google
 </button>
-          <p className='font-semibold'>Already have an account? <span className='text-secondary font-bold '><Link to="/auth/login">Login</Link></span></p>
-        </fieldset>
 
-        
-        {
-          error && <p className='text-red-500'>{error}</p>
-        }
-      </form>
+                <p className='font-semibold text-2xl'>
+                  Already have an account? 
+                  <span className='text-secondary font-bold'>
+                    <Link to="/auth/login"> Login</Link>
+                  </span>
+                </p>
+
+              </fieldset>
+
+              {error && <p className='text-red-500'>{error}</p>}
+
+            </form>
+          </div>
+        </div>
+
       </div>
     </div>
-    </div>
-    <div className='-mt-20'>
-        <Footer></Footer>
-    </div>
-    </div>
+
+  </div>
+
+ <div className='-mt-10'>
+     <Footer />
+ </div>
+</div>
     );
 };
 
